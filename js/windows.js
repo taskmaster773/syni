76f107b6776f7ae660e07525238ceebbfa430ccf
taskmaster773 <georgierodriguez0@gmail.com>
2026-09-20 02:19:12 -0500

    Change files loading to use promise for HTML

--- js/windows.js
+++ js/windows.js
@@ -32,7 +32,9 @@ window.openWindow = function(id){
     var f = document.getElementById('frame-' + id);
     if(f && dat.internal){
       if(id === 'settings') f.srcdoc = getSettingsHTML();
-      else if(id === 'files') f.srcdoc = getPS5EmuHTML();
+      else if(id === 'files'){
+  getPS5EmuHTML().then(function(html){ f.srcdoc = html; });
+}
       else if(id === 'term')  f.srcdoc = getSpotifyHTML();
       else if(id === 'proxy') f.srcdoc = getProxyBrowserHTML();
       else if(id === 'browser'){
