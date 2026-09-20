0f726b4d24b360dccf06aa1ddc18b42917deefe5
taskmaster773 <georgierodriguez0@gmail.com>
2026-09-20 00:43:16 -0500

    Delete js directory

--- js/state.js
+++
@@ -1,6 +0,0 @@
-'browser': {
-  title: 'Browser',
-  internal: true,
-  icon: 'https://cdn-icons-png.flaticon.com/512/1006/1006771.png',
-  pinned: true
-}
--- js/windows.js
+++
@@ -1,7 +0,0 @@
-if (f && dat.internal) {
-  if (id === 'settings')     f.srcdoc = getSettingsHTML();
-  else if (id === 'files')   f.srcdoc = getPS5EmuHTML();
-  else if (id === 'term')    f.srcdoc = getSpotifyHTML();
-  else if (id === 'proxy')   f.srcdoc = getProxyBrowserHTML();
-  else if (id === 'browser') getBrowserHTML().then(function (html) { f.srcdoc = html; });
-}
